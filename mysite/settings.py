126358ebd4340963740d7824c27015c2e28b4404
quit9to5 <vivacity.legend@gmail.com>
2015-06-27 07:08:20 +0530

    changed debug mode to False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#)+e%+fo-$7h0_(!f2dixi*pem!m*brc5s%em8ukilureg%j$x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
